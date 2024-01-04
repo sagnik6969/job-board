d92cd7ab887658bb4e803417b36c00fabe77741d
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-01-04 12:28:52 +0530

    126. Authentication: Sign-in Logic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,7 @@
 
 <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
     <div>
+        {{ auth()->user()->name ?? 'Guest' }}
         {{ $slot }}
     </div>
     {{-- similar to virw slots --}}
