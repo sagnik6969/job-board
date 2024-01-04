34b9bda733c5fbddc9f71f3dd91b775bd4d6f239
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-01-04 08:26:39 +0530

    121. Employer: Factory & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(300)->create();
+
+        $users = User::all()->shuffle();
+
+        for($i = 0 ; $i < 20 ; $i++){
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for($i = 0 ; $i < 100 ; $i++){
+            Job::factory()->create([
+               'employer_id' => $employers->random()->id
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Job::factory(100)->create();
+        // Job::factory(100)->create();
     }
 }
